b064c79002d0496855d1554c520fc5095c8f06fe
Parth Antala <52197140+parallel-appy@users.noreply.github.com>
2024-09-30 13:11:03 -0400

    redeign saved stories view

--- Imagine Tales/Library/Lib Views/SavedStoryView.swift
+++ Imagine Tales/Library/Lib Views/SavedStoryView.swift
@@ -13,6 +13,18 @@ struct SavedStoryView: View {
     @AppStorage("childId") var childId: String = "Default Value" // Stores the child's ID in user defaults
     @Binding var reload: Bool // Binding to trigger view updates when the value changes
     @Environment(\.colorScheme) var colorScheme
+    
+    // Define the grid layout with two columns
+        let columns = [
+            GridItem(.flexible()),
+            GridItem(.flexible())
+        ]
+    
+    @State private var retryCount = 0 // Count for retry attempts when loading images
+    @State private var maxRetryAttempts = 3 // Maximum number of retry attempts
+    @State private var retryDelay = 2.0 // Delay between retries
+    @State private var selectedStory: Story? = nil
+    
     var body: some View {
         NavigationStack {
             // Check if there are no saved stories
@@ -28,53 +40,109 @@ struct SavedStoryView: View {
                 }
                 .listRowBackground(Color.clear) // Clear background for the unavailable content view
             }
-            // List to display saved stories, each identified by its unique ID
-            List(viewModel.savedStories, id: \.id) { story in
-                // Navigation link to view details of the saved story
-                NavigationLink(destination: StoryFromProfileView(story: story)) {
-                    ZStack {
-                        HStack {
-                            // First background image for the story theme
-                            Image("\(story.theme?.filter { !$0.isWhitespace } ?? "")1")
-                                .resizable() // Make the image resizable
-                                .scaledToFit() // Scale the image to fit its container
-                                .opacity(colorScheme == .dark ? 0.7 : 0.3) // Set opacity for a translucent effect
-                                .frame(width: 300, height: 300) // Fixed dimensions for the image
-                            Spacer() // Spacer to push the second image to the right
-                            // Second background image with reduced size and opacity
-                            Image("\(story.theme?.filter { !$0.isWhitespace } ?? "")2")
-                                .resizable()
-                                .scaledToFit()
-                                .opacity(colorScheme == .dark ? 1 : 0.7) // Higher opacity than the first image
-                                .frame(width: 70, height: 70) // Smaller fixed dimensions
-                            Spacer() // Spacer to ensure proper layout
-                        }
-                        .frame(height: 100) // Set the height for the HStack containing the images
-                        
-                        // HStack for the main content of the story
-                        HStack {
-                            VStack {
-                                // Title of the story displayed prominently
-                                Text("\(story.title)")
-                                    .font(.custom("ComicNeue-Bold", size: 32)) // Custom font style for the title
-                                    .padding([.leading, .bottom]) // Padding around the title text
+            
+            ScrollView {
+                        LazyVGrid(columns: columns, spacing: 23) {
+                            ForEach(viewModel.savedStories, id: \.id) { story in
+                                ZStack {
+                                    // Load the story image asynchronously
+                                    AsyncImage(url: URL(string: story.storyText[0].image)) { phase in
+                                        switch phase {
+                                        case .empty:
+                                            MagicView()
+                                                .frame(width: UIScreen.main.bounds.width * 0.45, height: 500)
+                                            
+                                        case .success(let image):
+                                            // Successfully loaded image
+                                            image
+                                                .resizable()
+                                                .scaledToFill()
+                                                .frame(width: UIScreen.main.bounds.width * 0.45, height: 500)
+                                                .clipped()
+                                                .cornerRadius(16)
+                                            
+                                        case .failure(_):
+                                            // Placeholder for failed load
+                                            Image(systemName: "photo")
+                                                .resizable()
+                                                .scaledToFit()
+                                                .frame(width: UIScreen.main.bounds.width * 0.45, height: 500)
+                                                .cornerRadius(16)
+                                                .padding()
+                                                .onAppear {
+                                                    // Retry loading logic
+                                                    if retryCount < maxRetryAttempts {
+                                                        DispatchQueue.main.asyncAfter(deadline: .now() + retryDelay) {
+                                                            retryCount += 1
+                                                        }
+                                                    }
+                                                }
+                                        @unknown default:
+                                            EmptyView()
+                                        }
+                                    }
+                                    .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 5) // Add shadow to the image
+                                    
+                                    VStack {
+                                        Spacer()
+                                        
+                                        ZStack {
+                                            RoundedRectangle(cornerRadius: 0)
+                                                .fill(Color.white.opacity(0.8))
+                                                .frame(width: UIScreen.main.bounds.width * 0.40, height: 200)
+                                                .cornerRadius(16)
+                                            
+                                            VStack(spacing: 0) {
+                                                
+                                                Text(story.title)
+                                                    .font(.system(size: 18))
+                                                Text("By \(story.childUsername)")
+                                                    .font(.system(size: 16))
+                                                    .padding(.top, -20)
+                                                HStack {
+                                                    Image(systemName: "heart.fill")
+                                                       
+                                                    Text("Likes")
+                                                        .padding(.trailing)
+                                                    
+                                                    Text(story.theme ?? "")
+                                                        .padding(7)
+                                                        .background(colorScheme == .dark ? Color(hex: "#4B8A1C") : .green)
+                                                        .foregroundStyle(.white)
+                                                        .cornerRadius(22)
+                                                        
+                                                }
+                                                .font(.system(size: 16))
+                                                .padding(.top)
+                                                Button {
+                                                    selectedStory = story
+                                                } label: {
+                                                    HStack {
+                                                        Text("Read Now")
+                                                        Image(systemName: "book.pages")
+                                                    }
+                                                    .frame(width: UIScreen.main.bounds.width * 0.35)
+                                                }
+                                                .padding()
+                                                .font(.system(size: 16))
+                                                .background(Color(hex: "#FF6F61"))
+                                                .foregroundStyle(.white)
+                                                .cornerRadius(16)
+                                                .padding(.top)
+                                            }
+                                            .foregroundStyle(.black)
+                                            
+                                        }
+                                        .padding(.bottom, 10)
+                                    }
+                                }
                             }
-                            Spacer() // Spacer to align title to the left
                         }
-                        .contentShape(Rectangle()) // Define the tappable area for the HStack
+                        .padding()
                     }
-                    // Padding and background for the list item
-                    .padding(.vertical) // Vertical padding for the list item
-                    .background(colorScheme == .dark ? Color.black.opacity(0.4) : .white.opacity(0.4)) // Semi-transparent white background
-                    .cornerRadius(22) // Rounded corners for the list item
-                    .shadow(color: Color.black.opacity(0.2), radius: 10, x: 10, y: 0)
-                    .contentShape(Rectangle()) // Define the tappable area for the overall item
-                }
-                .buttonStyle(.plain) // Use a plain button style for a consistent look
-                .listRowBackground(Color.white.opacity(0)) // Transparent background for the list row
-                .listRowSeparator(.hidden) // Hide the separator between rows
+            .fullScreenCover(item: $selectedStory) { story in
+                    StoryFromProfileView(story: story)
             }
-            .scrollContentBackground(.hidden) // Hide the default scroll background
             .navigationTitle("Saved Stories") // Set the title of the navigation bar
             .onAppear {
                 // Fetch saved stories when the view appears
@@ -84,6 +152,7 @@ struct SavedStoryView: View {
                 // Fetch saved stories again when reload is triggered
                 viewModel.getSavedStories(forChild: childId)
             }
+
         }
     }
 }
