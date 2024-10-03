edac71cb73283d867f5a87860f90b77801c83702
Parth Antala <52197140+parallel-appy@users.noreply.github.com>
2024-10-02 22:16:18 -0400

    update parent setting view

--- Imagine Tales/Parent/Parent Views/parentSettings.swift
+++ Imagine Tales/Parent/Parent Views/parentSettings.swift
@@ -22,28 +22,36 @@ struct parentSettings: View {
     func reset() async throws {
         try await AuthenticationManager.shared.resetPassword(email: enteredEmail.lowercased())
     }
+    @Environment(\.colorScheme) var colorScheme
+    
     var body: some View {
         NavigationStack {
             ZStack {
                 BackGroundMesh() // Custom background for the view
                 VStack {
-                    List {
-                        HStack {
-                            if !isEditingName {
-                                Text(viewModel.parent?.name ?? "Loading...")
-                                Spacer()
-                                Button("Edit") {
+                    HStack {
+                        if !isEditingName {
+                            Text(viewModel.parent?.name ?? "Loading...")
+                            Spacer()
+                            Button {
+                                withAnimation {
                                     isEditingName.toggle()
                                 }
-                            } else {
-                                TextField("", text: $newName)
-                                Button("Save") {
+                            } label: {
+                                Image(systemName: "pencil.line")
+                            }
+                        } else {
+                            TextField("\(viewModel.parent?.name ?? "Loading...")", text: $newName)
+                            HStack {
+                                Button {
                                     viewModel.updateNameInFirestore(userId: viewModel.parent?.userId ?? "", newName: newName) { result in
                                         switch result {
                                         case .success:
                                             print("Name updated successfully.")
                                             Drops.show("Name updated successfully.")
-                                            isEditingName.toggle()
+                                            withAnimation {
+                                                isEditingName.toggle()
+                                            }
                                             do {
                                                 try viewModel.fetchParent()
                                             } catch {
@@ -53,63 +61,131 @@ struct parentSettings: View {
                                             print("Error updating name: \(error.localizedDescription)")
                                         }
                                     }
+                                } label: {
+                                    Image(systemName: "checkmark.circle.fill")
+                                        .font(.system(size: 22))
                                 }
-                            }
-                            
-                            
-                            
-                        }
-                        
-                        if !isResettingPassword {
-                            Button("Reset password") {
-                                isResettingPassword.toggle()
-                            }
-                        } else {
-                            
-                            TextField("Enter your email", text: $enteredEmail)
-                            
-                            
-                            
-                            if enteredEmail.lowercased() == viewModel.parent?.email {
-                                Button("Reset") {
-                                    Task {
-                                        do {
-                                            try await reset()
-                                            Drops.show("Check your email for a reset link")
-                                            isResettingPassword.toggle()
-                                        } catch {
-                                            
-                                        }
-                                        
+                                
+                                Button {
+                                    withAnimation {
+                                        isEditingName.toggle()
                                     }
-                                }
-                            } else {
-                                if !enteredEmail.isEmpty {
-                                    Text("Invalid Email")
-                                        .foregroundStyle(.red)
-                                }
-                                Button("Cancel") {
-                                    isResettingPassword.toggle()
-                                }
-                            }
-                        }
-                    
-                        
-                        // Button to log out the parent
-                        Button("Log out") {
-                            Task {
-                                do {
-                                    try viewModel.logOut() // Attempt to log out
-                                    showSigninView = true // Set binding to show sign-in view
-                                    dismiss() // Dismiss the settings view
-                                } catch {
-                                    // Print error if logout fails
-                                    print(error.localizedDescription)
+                                } label: {
+                                    Image(systemName: "x.circle.fill")
+                                        .foregroundStyle(.orange)
+                                        .font(.system(size: 22))
                                 }
                             }
                         }
                     }
+                    .padding()
+                    .frame(width: UIScreen.main.bounds.width * 0.5)
+                    .background(colorScheme == .dark ? .black.opacity(0.2) : .white)
+                    .cornerRadius(12)
+                    
+                    
+                    if !isResettingPassword {
+                        Button {
+                            withAnimation {
+                                isResettingPassword.toggle()
+                            }
+                        } label: {
+                            Text("Reset Passowrd")
+                                .padding()
+                                .frame(width: UIScreen.main.bounds.width * 0.5)
+                                .background(colorScheme == .dark ? .black.opacity(0.2) : .white)
+                                .cornerRadius(12)
+                        }
+                        
+                    } else {
+                        
+                        TextField("Enter your email", text: $enteredEmail)
+                            .padding()
+                            .frame(width: UIScreen.main.bounds.width * 0.5)
+                            .background(colorScheme == .dark ? .black.opacity(0.2) : .white)
+                            .cornerRadius(12)
+                        
+                        HStack {
+                            Button {
+                                let parentEmail = viewModel.parent?.email?.lowercased().trimmingCharacters(in: .whitespacesAndNewlines) ?? ""
+                                let enteredEmailTrimmed = enteredEmail.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+                                
+                                if enteredEmailTrimmed == parentEmail {
+                                    Task {
+                                        do {
+                                            try await reset()
+                                            Drops.show("Check your email for a reset link")
+                                            withAnimation {
+                                                isResettingPassword.toggle()
+                                            }
+                                        } catch {
+                                            // Handle error
+                                        }
+                                    }
+                                } else {
+                                    if !enteredEmail.isEmpty {
+                                        Drops.show("Please enter a valid email.")
+                                    } else {
+                                        Drops.show("Please enter a your email.")
+                                    }
+                                }
+                                
+                                
+                                
+                            } label: {
+                                Text("Reset Password")
+                                    .padding()
+                                    .frame(width: UIScreen.main.bounds.width * 0.3)
+                                    .background(colorScheme == .dark ? .black.opacity(0.2) : .white)
+                                    .cornerRadius(12)
+                                
+                            }
+                            
+                            Button {
+                                withAnimation {
+                                    isResettingPassword.toggle()
+                                }
+                            } label: {
+                                Text("Cancel")
+                                    .padding()
+                                    .frame(width: UIScreen.main.bounds.width * 0.17)
+                                    .background(colorScheme == .dark ? .black.opacity(0.2) : .white)
+                                    .foregroundStyle(.red)
+                                    .cornerRadius(12)
+                            }
+                        }
+                        
+                       
+                        
+                        
+                        
+                    }
+                
+                    
+                    // Button to log out the parent
+                    Button {
+                        Task {
+                            do {
+                                try viewModel.logOut() // Attempt to log out
+                                showSigninView = true // Set binding to show sign-in view
+                                dismiss() // Dismiss the settings view
+                            } catch {
+                                // Print error if logout fails
+                                print(error.localizedDescription)
+                            }
+                        }
+                    } label: {
+                        Text("Log Out")
+                            .padding()
+                            .frame(width: UIScreen.main.bounds.width * 0.5)
+                            .background(colorScheme == .dark ? .black.opacity(0.2) : .white)
+                            .foregroundStyle(.red)
+                            .cornerRadius(12)
+                    }
+                    
+                    Spacer()
                 }
+                .padding()
                 .onAppear {
                     do {
                         try viewModel.fetchParent()
